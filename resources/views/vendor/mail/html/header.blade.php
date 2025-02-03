f1d0e8d339ddbaea221fefb1e38a3c8dd6d274a6
lunetajoredz <0rw2fmscs51ljzd0y2work4rdkwagu@bots.bitbucket.org>
2025-02-03 15:09:00 +0800

    update

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
+<a href="https://hsp.joredz.com" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
 <img src="https://hsp.joredz.com/storage/logo.png" class="rounded-lg" width="50px" height="50px;" alt="" title="" />
 @else
