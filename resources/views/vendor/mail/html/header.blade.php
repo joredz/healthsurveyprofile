5a58b6a3df91df8c517710929379847b6d778239
lunetajoredz <0rw2fmscs51ljzd0y2work4rdkwagu@bots.bitbucket.org>
2025-02-03 14:50:25 +0800

    update verify email

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,12 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="{{ url('storage/logo.png') }}" class="rounded-lg" width="50px" height="50px;" alt="" title="" />
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>
